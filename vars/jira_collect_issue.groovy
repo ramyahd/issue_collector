8e3912bfed4297bcaf942a007add405dede4c4c7
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 19:34:08 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -5,12 +5,12 @@ collectissues(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def projectName = '"'+resultJson.key+'"'
-
+echo "$projectName"
  httpRequest authentication: 'jira_password', 
     customHeaders:[[maskValue: false, name: 'Accept', value: 'application/json']], 
     httpMode: 'GET', requestBody: """{
     "name": ${projectName},
-}""", responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project="${projectName}"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"'
+}""", responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project=${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"'
 
  
 }
