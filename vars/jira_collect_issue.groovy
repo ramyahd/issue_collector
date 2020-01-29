5b84efcfbd53f58729758335549373326fa402d9
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 21:58:37 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -9,7 +9,7 @@ echo "$projectName"
  httpRequest authentication: 'jira_password',
   customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
                     [maskValue: false, name: 'Accept', value: 'application/json']], 
-     httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
+     httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
      connection.setRequestMethod('GET')
      def connection = url.toURL().openConnection()
    
