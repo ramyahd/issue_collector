946f6a52f2826e6412895fb81a9d368074562436
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 15:52:37 +0530

    Update jira_issue_collector.groovy

--- vars/jira_issue_collector.groovy
+++ vars/jira_issue_collector.groovy
@@ -5,7 +5,9 @@ collectissue(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def projectKey = '"'+resultJson.key+'"'
-
+ 
+ echo ${projectKey}
+ 
 sh '''
  curl -X GET \
   'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project="${projectKey}"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0' \
