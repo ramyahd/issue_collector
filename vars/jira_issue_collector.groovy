e15a34c4ce6333d38e6db830a31280f804e30eda
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 18:26:09 +0530

    Update jira_issue_collector.groovy

--- vars/jira_issue_collector.groovy
+++ vars/jira_issue_collector.groovy
@@ -8,7 +8,7 @@ def projectKey = '"'+resultJson.key+'"'
  
 
 sh '''
- println "${projectKey}"
+ echo "${projectKey}"
  curl  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz'  -X GET \
   "http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project="$projectKey"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0" \
   -H 'accept: application/json' \
