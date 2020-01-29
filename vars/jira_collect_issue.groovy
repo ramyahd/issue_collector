cf1b02c57bfe7ab97717a1b8a8e144969701dc25
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 21:50:10 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -10,8 +10,16 @@ echo "$projectName"
   customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
                     [maskValue: false, name: 'Accept', value: 'application/json']], 
     httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
-
- 
+     def connection = url.toURL().openConnection()
+   
+  try {
+      connection.connect()
+     def line = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine()
+     def jsonSlurper = new JsonSlurper()
+     return jsonSlurper.parseText(line)
+  } finally {
+        connection.disconnect();
+    }
 }
 def call(){
  def request = libraryResource 'data1.json'
