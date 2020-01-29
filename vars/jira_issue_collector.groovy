5d5f049533f5c4133b5d0bc9b9825c390d510e1e
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 16:40:15 +0530

    Update jira_issue_collector.groovy

--- vars/jira_issue_collector.groovy
+++ vars/jira_issue_collector.groovy
@@ -10,10 +10,9 @@ def projectKey = '"'+resultJson.key+'"'
  
 sh '''
  curl  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz'  -X GET \
-  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project="$.projectKey"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0' \
+  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project="${projectKey}"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0' \
   -H 'accept: application/json' \
   -H 'cache-control: no-cache' \
-  -H 'postman-token: c1a72319-7788-9398-41a5-6901d8ea91fd'
  '''
  }
  
