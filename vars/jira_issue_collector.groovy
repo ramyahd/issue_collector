10c1c9b80853e26d235b36c541c49d3dc55c2265
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-29 19:12:08 +0530

    Update jira_issue_collector.groovy

--- vars/jira_issue_collector.groovy
+++ vars/jira_issue_collector.groovy
@@ -7,11 +7,8 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parseText(data)
 def projectKey = '"'+resultJson.key+'"'
  
- curl  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz'  -X GET \
-  "http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project="$projectKey"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0" \
-  -H 'accept: application/json' \
-  -H 'cache-control: no-cache' \
-'''
+response=$(curl  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz'  -X GET \
+  "http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=&project="$projectKey"&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0")
  }
  
  def call(){
