bcf9092f3e74085ed20f077ddd9910047ae3dd13
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-30 18:02:45 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -1,4 +1,6 @@
 import groovy.json.JsonSlurper 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @NonCPS
 collectissues(String data){
@@ -9,15 +11,9 @@ echo "$projectName"
  httpRequest authentication: 'jira_password',
   customHeaders: [[maskValue: false, name: 'Accept', value: 'application/json']], 
      httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
-     def connection = url.toURL().openConnection()
-   
-  try {
-      connection.connect()
-     def line = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine()
-     return jsonSlurper.parseText(line)
-  } finally {
-        connection.disconnect();
-    }
+
+final Logger logger = LoggerFactory.getLogger("myGroovyLogger");
+logger.trace("Test log output message");
 }
 def call(){
  def request = libraryResource 'data1.json'
