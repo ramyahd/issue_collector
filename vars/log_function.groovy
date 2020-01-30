dd586d066d29d28adf14f7ab5ec9dfad5fad8ceb
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-30 19:36:42 +0530

    Update log_function.groovy

--- vars/log_function.groovy
+++ vars/log_function.groovy
@@ -5,7 +5,7 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parseText(data)
 def projectName = resultJson.key
 
-  echo '"$projectName">jira.txt'
+  echo "${projectName}" > jira.txt
 }
 def call(){
 def request = libraryResource 'data1.json'
