2ada6532633e37652c06ecda4c1601f60583b2fd
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-30 18:52:45 +0530

    Update log_function.groovy

--- vars/log_function.groovy
+++ vars/log_function.groovy
@@ -1,1 +1,9 @@
-echo 'hi>jira.txt'
+  
+import groovy.json.JsonSlurper 
+@NonCPS
+collectissues(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projectName = resultJson.key
+
+  echo "Project created with $projectName in JIRA">'jira.txt'
