d357c4c141f73b466b9c268e38c5f1592bd23bb9
Anushkashubhi <55789857+Anushkashubhi@users.noreply.github.com>
2020-01-30 18:23:25 +0530

    removed
      try {
          connection.connect()
         def line = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine()
         return jsonSlurper.parseText(line)
      } finally {
            connection.disconnect();
        }

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -10,14 +10,7 @@ echo "$projectName"
   customHeaders: [[maskValue: false, name: 'Accept', value: 'application/json']], 
      httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
      def connection = url.toURL().openConnection()
-   
-  try {
-      connection.connect()
-     def line = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine()
-     return jsonSlurper.parseText(line)
-  } finally {
-        connection.disconnect();
-    }
+ 
 }
 def call(){
  def request = libraryResource 'data1.json'
