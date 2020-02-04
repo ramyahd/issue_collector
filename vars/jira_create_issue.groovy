5dd80c55ab6d5b98b465bbc8b6b499df8289430c
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-04 15:11:42 +0530

    Update jira_create_issue.groovy

--- vars/jira_create_issue.groovy
+++ vars/jira_create_issue.groovy
@@ -19,19 +19,20 @@ def projLength = resultJson.name.size()
    }
    appendStr=appendStr+resultJson.name.substring(0, Math.min(projectName.length(), len));
    key=appendStr.toUpperCase();
- }
+   }
  def projKey = '"'+key+'"'
  println projKey   
     
-httpRequest authentication: 'jira_password', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: '''{
-    "fields": {
+httpRequest authentication: 'jira_password',
+ customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'],
+                 [maskValue: false, name: 'Accept', value: 'application/json']], 
+ httpMode: 'POST', requestBody: '''{
        "key": ${projKey},
        "summary": "this is summary 1",
        "description": "this is description",
        "issuetype": {
           "name": "Task"
        }
-   }
 }''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/'
 }
 
