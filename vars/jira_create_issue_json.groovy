68d9724ccede7163e9da0d98988200c040ab0eed
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-04 19:56:00 +0530

    Update jira_create_issue_json.groovy

--- vars/jira_create_issue_json.groovy
+++ vars/jira_create_issue_json.groovy
@@ -1,1 +1,49 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
 
+String a=jsonObj.alm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+env.name = projectName
+  
+def length = 3
+def projLength = name.size()
+ println(projLength)
+ if(projLength>=3){
+  key=name.substring(0, Math.min(projLength, length)).toUpperCase();
+ }
+ else {
+   def appendStr = "";
+   int currentLength=length;
+   int len = currentLength%projLength;
+   int repeat = currentLength/projLength;
+   for (int i=0;i<repeat;i++) {
+    appendStr = appendStr + name;
+   }
+   appendStr=appendStr+name.substring(0, Math.min(projectName.length(), len));
+   key=appendStr.toUpperCase();
+   }
+ def projKey = key
+ 
+ println(projKey)
+    
+httpRequest authentication: 'jira_password',
+ customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'],
+                 [maskValue: false, name: 'Accept', value: 'application/json']], 
+ httpMode: 'POST', requestBody: """{
+     "fields": {
+       "project":
+       {
+          "key": "${projKey}"
+       },
+       "summary": "this is summary 1",
+       "description": "this is description",
+       "issuetype": {
+          "name": "Task"
+       }
+   }
+}""", url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/'
+}
