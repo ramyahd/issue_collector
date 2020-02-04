b58c4713bf37fe74136da49b7a2e403fa74fa750
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-04 15:15:31 +0530

    Update jira_create_issue.groovy

--- vars/jira_create_issue.groovy
+++ vars/jira_create_issue.groovy
@@ -27,12 +27,17 @@ httpRequest authentication: 'jira_password',
  customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'],
                  [maskValue: false, name: 'Accept', value: 'application/json']], 
  httpMode: 'POST', requestBody: '''{
-       "key": ${projKey},
+     "fields": {
+       "project":
+       {
+          "key": ${projKey}
+       },
        "summary": "this is summary 1",
        "description": "this is description",
        "issuetype": {
           "name": "Task"
        }
+   }
 }''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/'
 }
 
