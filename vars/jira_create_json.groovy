5bc1b3644a3369d16596a91e5114291918d8c771
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-04 19:26:11 +0530

    Update jira_create_json.groovy

--- vars/jira_create_json.groovy
+++ vars/jira_create_json.groovy
@@ -12,7 +12,7 @@ String b=jsonObj.alm.projects.project.project_typeKey
 //String b=jsonObj.alm.projects.project.projectTypeKey
  String projectTypeKey=b.replaceAll("\\[", "").replaceAll("\\]","");
 
-String c=jsonObj.alm.projects.project.project_lead
+String c=jsonObj.alm.projects.project.projectLead
 //String c=jsonObj.alm.projects.project.lead
  String lead=a.replaceAll("\\[", "").replaceAll("\\]","");
 
