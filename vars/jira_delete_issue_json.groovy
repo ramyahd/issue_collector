a85583ab2e27bfa34d04634a03d8833e2a736028
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 17:11:19 +0530

    Create jira_delete_issue_json.groovy

---
+++ vars/jira_delete_issue_json.groovy
@@ -0,0 +1,7 @@
+curl -X DELETE \
+  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/EDN-1 \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: d93188c9-8f17-5dff-7177-98ecad39a3ef'
