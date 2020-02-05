5a22eff0d1197eb95ab763d4b3c4621b03bdb52c
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 12:36:02 +0530

    Create jira_delete_project.groovy

---
+++ vars/jira_delete_project.groovy
@@ -0,0 +1,8 @@
+curl -X DELETE \
+  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/EDN \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 106b5efa-4bcf-e924-b895-abda8ffe17a7'
+  
