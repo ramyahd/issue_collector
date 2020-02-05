9c2717e4d334e273c049ff1ef86661d23ba4fbb7
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 12:57:00 +0530

    Update jira_delete_project.groovy

--- vars/jira_delete_project.groovy
+++ vars/jira_delete_project.groovy
@@ -36,14 +36,7 @@ def projLength = name.size()
  httpRequest authentication: 'jira_password',
  customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'],
                  [maskValue: false, name: 'Accept', value: 'application/json']], 
- httpMode: 'DELETE', requestBody: """{
-     "fields": {
-       "project":
-       {
-          "key": "${projKey}"
-       }
-   }
-}""", url: ' http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/${projKey}'
+ httpMode: 'DELETE',  url: "http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/${projKey}"
 }
     
 /*curl -X DELETE \
