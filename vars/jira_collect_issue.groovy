0139ea223be896e5d72d11e59dfa9b93afd22137
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 16:43:35 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -12,6 +12,15 @@ echo "$projectName"
   customHeaders: [[maskValue: false, name: 'Accept', value: 'application/json']], 
      httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
 
+ echo "sh '''
+ curl -X GET \
+  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3DAVR&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: c1a72319-7788-9398-41a5-6901d8ea91fd'
+ '''"
+ }
 }
 
 
