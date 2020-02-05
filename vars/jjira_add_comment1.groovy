998e6668a365ed15f5a1441eeb442e8491bde367
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 18:21:19 +0530

    Update jjira_add_comment1.groovy

--- vars/jjira_add_comment1.groovy
+++ vars/jjira_add_comment1.groovy
@@ -1,3 +1,15 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
+
+String a=jsonObj.alm.projects.project.issue_name
+String issueName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+env.name = issueName
+
+
 curl --request POST \
   --url http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/AVR-6/comment \
   --header 'accept: application/json' \
