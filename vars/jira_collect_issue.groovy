9ee360f8352dc5365db6ac51ab8fb0bdec408608
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 19:10:35 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -27,25 +27,26 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 env.name = projectName
 
- sh """curl -X POST \
+/* sh """curl -X POST \
   http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -H 'postman-token: 03a3b589-c1d3-4234-8043-9335bf162947' \
-  -d '{"jql":"project = EDN","startAt":0,"maxResults":2,"fields":["id","key"]}'node <<< "var o = $(cat); console.log(JSON.stringify(o, null, 4));
- 
+  -d '{"jql":"project = EDN","startAt":0,"maxResults":2,"fields":["id","key"]}'
  """
- 
-/*sh """curl -X GET \
+ */
+sh """curl -X GET \
  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20order%20by%20duedate&fields=id%2Ckey' \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
+  node <<< "var o = $(cat); console.log(JSON.stringify(o, null, 4));
+ 
 """
-}*/
+}
 /*def call(){
  def request = libraryResource 'data1.json'
  collectissues(request)
