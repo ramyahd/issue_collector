8dfda018678713d671ab636d6bcb80542472c353
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 18:22:16 +0530

    Update jjira_add_comment1.groovy

--- vars/jjira_add_comment1.groovy
+++ vars/jjira_add_comment1.groovy
@@ -10,11 +10,12 @@ String issueName=a.replaceAll("\\[", "").replaceAll("\\]","");
 env.name = issueName
 
 
-curl --request POST \
-  --url http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/AVR-6/comment \
+sh """curl --request POST \
+  --url http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/${issueName}/comment \
   --header 'accept: application/json' \
   --header 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   --header 'cache-control: no-cache' \
   --header 'content-type: application/json' \
   --header 'postman-token: 9cb64ee0-8c96-3d32-94b7-7c2005e1f495' \
-  --data '{\n    "body": "This is a comment regarding the quality of the response."\n}'
+  --data '{\n    "body": "This is a comment regarding the quality of the response."\n}'"""
+}
