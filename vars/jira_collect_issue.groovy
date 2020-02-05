0d25256e30a9f095205a3aade8a50aa5463b1f0f
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 18:37:54 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -1,5 +1,5 @@
-import groovy.json.JsonSlurper 
-import org.slf4j.Logger;
+//import groovy.json.JsonSlurper 
+/*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @NonCPS
@@ -15,8 +15,28 @@ echo "$projectName"
  }
 }
 
+*/
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
 
-def call(){
+String a=jsonObj.alm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+env.name = projectName
+
+sh """curl -X GET \
+ 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20order%20by%20duedate&fields=id%2Ckey' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+"""
+}
+/*def call(){
  def request = libraryResource 'data1.json'
  collectissues(request)
 }
+*/
