a3b0be4d1d547aa6298aa488715cb7529fcb7c65
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-05 19:11:28 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -38,7 +38,7 @@ env.name = projectName
  """
  */
 sh """curl -X GET \
- 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20order%20by%20duedate&fields=id%2Ckey' \
+ 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D"${projectName}"%20order%20by%20duedate&fields=id%2Ckey' \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
