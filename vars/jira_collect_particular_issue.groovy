51b7648b1b4b7c4219caaacbbb67496f205cdef5
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 12:57:29 +0530

    Update jira_collect_particular_issue.groovy

--- vars/jira_collect_particular_issue.groovy
+++ vars/jira_collect_particular_issue.groovy
@@ -12,11 +12,11 @@ env.name = issueName
 
 
 sh """
-curl --request GET \
-  --url http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/3/issue/${issueName} \
-  --header 'accept: application/json' \
-  --header 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
-  --header 'cache-control: no-cache' \
-  --header 'content-type: application/json' | json_reforamt
+curl -X GET \
+ 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/3/issue/${issueName}' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' | json_reformat
   """
   }
