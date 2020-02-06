522df679f47033ce1674b88143059562f8383ff9
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 19:32:24 +0530

    Update jira_summary_of_project.groovy

--- vars/jira_summary_of_project.groovy
+++ vars/jira_summary_of_project.groovy
@@ -32,7 +32,7 @@ def projLength = name.size()
 
 sh """
 curl -X GET \
-  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/%20EDN \
+  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/%20${projKey} \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
