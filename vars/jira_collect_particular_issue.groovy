f190800fbef135b94d51864c14330dfd9f4d4ca9
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 13:31:52 +0530

    Update jira_collect_particular_issue.groovy

--- vars/jira_collect_particular_issue.groovy
+++ vars/jira_collect_particular_issue.groovy
@@ -17,6 +17,6 @@ curl -X GET \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' | jq .fields."project"|map(.name)
+  -H 'content-type: application/json' | jq '.fields."project"|map(.name)'
   """
   }
