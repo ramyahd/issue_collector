af98e55ff6cc012983e3308fe3f91714e149764d
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 19:20:54 +0530

    Create jira_summary_of_project.groovy

---
+++ vars/jira_summary_of_project.groovy
@@ -0,0 +1,41 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
+
+String a=jsonObj.alm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+env.name = projectName
+
+def length = 3
+def projLength = name.size()
+ println(projLength)
+ if(projLength>=3){
+  key=name.substring(0, Math.min(projLength, length)).toUpperCase();
+ }
+ else {
+   def appendStr = "";
+   int currentLength=length;
+   int len = currentLength%projLength;
+   int repeat = currentLength/projLength;
+   for (int i=0;i<repeat;i++) {
+    appendStr = appendStr + name;
+   }
+   appendStr=appendStr+name.substring(0, Math.min(projectName.length(), len));
+   key=appendStr.toUpperCase();
+   }
+ def projKey = key
+ 
+ println(projKey)
+
+sh """
+curl -X GET \
+  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/%20EDN \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' | json_reformat
+  """
+  }
