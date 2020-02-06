06e520ab1c3ab9deecad872bcf1d3e81d3c32a9d
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 21:52:08 +0530

    Update jira_collect_all_listings.groovy

--- vars/jira_collect_all_listings.groovy
+++ vars/jira_collect_all_listings.groovy
@@ -3,7 +3,7 @@ def call()
 
 sh """
 curl -X GET \
-  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/dashboard \
+  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/dashboard' \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
