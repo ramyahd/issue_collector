27afb417af97926c89e41e21b6fa82467f031755
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 21:51:03 +0530

    Update jira_collect_all_listings.groovy

--- vars/jira_collect_all_listings.groovy
+++ vars/jira_collect_all_listings.groovy
@@ -7,6 +7,6 @@ curl -X GET \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' - o output.json
+  -H 'content-type: application/json' 
   """
   }
