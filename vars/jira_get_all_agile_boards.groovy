2845637073028ab77fd8db77a83769616796c5b8
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 22:04:13 +0530

    Update jira_get_all_agile_boards.groovy

--- vars/jira_get_all_agile_boards.groovy
+++ vars/jira_get_all_agile_boards.groovy
@@ -1,5 +1,6 @@
 def call()
 {
+sh """
 curl --request GET \
   --url http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/agile/1.0/board \
   --header 'accept: application/json' \
@@ -7,4 +8,5 @@ curl --request GET \
   --header 'cache-control: no-cache' \
   --header 'content-type: application/json' \
   --header 'postman-token: c56c57b3-4529-6938-19aa-a3184cc6c584'
-  }
+"""
+}
