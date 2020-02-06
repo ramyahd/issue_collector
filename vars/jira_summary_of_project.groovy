2ef9803ef0202b932e5322c5d6639bff5a394752
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 19:34:16 +0530

    Update jira_summary_of_project.groovy

--- vars/jira_summary_of_project.groovy
+++ vars/jira_summary_of_project.groovy
@@ -9,30 +9,9 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 env.name = projectName
 
-def length = 3
-def projLength = name.size()
- println(projLength)
- if(projLength>=3){
-  key=name.substring(0, Math.min(projLength, length)).toUpperCase();
- }
- else {
-   def appendStr = "";
-   int currentLength=length;
-   int len = currentLength%projLength;
-   int repeat = currentLength/projLength;
-   for (int i=0;i<repeat;i++) {
-    appendStr = appendStr + name;
-   }
-   appendStr=appendStr+name.substring(0, Math.min(projectName.length(), len));
-   key=appendStr.toUpperCase();
-   }
- def projKey = key
- 
- println(projKey)
-
 sh """
 curl -X GET \
-  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/%20${projKey} \
+  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project/%20${projName} \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
