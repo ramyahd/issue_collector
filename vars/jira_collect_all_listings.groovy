d1f64e4c08e32130301cf794a2f2561e6a26a118
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-06 21:47:40 +0530

    Create jira_collect_all_listings.groovy

---
+++ vars/jira_collect_all_listings.groovy
@@ -0,0 +1,12 @@
+def call()
+{
+
+sh """
+curl -X GET \
+  http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/dashboard \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' - o output.json
+  """
+  }
