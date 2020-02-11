fefde8eaf901e3ca454862b9d91cd036d2cb0f81
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 18:31:15 +0530

    Update Move_to_influx.groovy

--- vars/Move_to_influx.groovy
+++ vars/Move_to_influx.groovy
@@ -8,7 +8,8 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson.total
 echo 'total'
-curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira,issues="${total}"' 
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira,issues='${total}'' 
+"""
 }
 
 
