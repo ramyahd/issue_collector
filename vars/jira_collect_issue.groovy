fe3210815ac895a41cfb6c86d79562a633c54e79
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 19:44:24 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -15,7 +15,7 @@ import groovy.json.*
 
 
 
-def call(jsondata){
+def issue(jsondata){
 def jsonString = jsondata
 //println(jsonString)
 def jsonObj = readJSON text: jsonString
@@ -47,11 +47,29 @@ def process=sh """curl -X GET \
  
 """
  
+} 
+
+@NonCPS
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def total = resultJson.total
+  echo "$total"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${total}' 
+"""
+}
+
+
+def call()
+{
+create()
+}
  
 //  HTTP_STATUS='$(echo "${process}" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')'
  //echo '$HTTP_STATUS
  //echo "$process"
-}
+
 /*def call(){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
