1468d2d989b78e931b51d2e395881210d43debb6
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 17:46:18 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -1,9 +1,9 @@
-import groovy.json.JsonSlurper 
+import groovy.json.* 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
-@NonCPS
-collectissues(String projectName){
+/*@NonCPS
+//collectissues(String projectName){
 //def jsonSlurper = new JsonSlurper() 
 //def resultJson = jsonSlurper.parseText(data)
 //def projectName = resultJson.key
@@ -12,11 +12,11 @@ collectissues(String projectName){
   customHeaders: [[maskValue: false, name: 'Accept', value: 'application/json']], 
      httpMode: 'GET', url:"http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0"
 
- }
+ }*/
 
 
 
-/*def call(jsondata){
+def call(jsondata){
 def jsonString = jsondata
 //println(jsonString)
 def jsonObj = readJSON text: jsonString
@@ -27,7 +27,7 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 env.name = projectName
 
-/*sh '''curl -X GET \
+sh '''curl -X GET \
   http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
@@ -36,7 +36,7 @@ env.name = projectName
   -H 'postman-token: 03a3b589-c1d3-4234-8043-9335bf162947' \
   -d '{"jql":"project = EDN","startAt":0,"maxResults":2,"fields":["id","key"]} '| json_reformat
  '''
- */
+ 
 /*sh """curl -X GET \
  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20order%20by%20duedate&fields=id%2Ckey' \
   -H 'accept: application/json' \
@@ -46,7 +46,7 @@ env.name = projectName
  
 """
 }*/
-def call(){
+/*def call(){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 //println(jsonObj.alm)
@@ -55,6 +55,6 @@ String a=jsonObj.alm.projects.project.project_name
 String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 collectissues(projectName)
-}
+}*/
 
 
