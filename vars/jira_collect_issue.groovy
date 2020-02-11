0da6e26486324391f7f2ecc38569b1ca05869079
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 17:52:39 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -41,7 +41,7 @@ sh """curl -X GET \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
-  -H 'content-type: application/json'  | json_reformat
+  -H 'content-type: application/json' -o ouput.json | json_reformat
  
 """
 }
