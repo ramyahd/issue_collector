a2a0849354989d382d9f2a9723045ab2527245fa
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 18:38:00 +0530

    Update Move_to_influx.groovy

--- vars/Move_to_influx.groovy
+++ vars/Move_to_influx.groovy
@@ -7,7 +7,7 @@ def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/body.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson.total
-  echo 'total'
+  echo "$total"
 sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira,issues="${total}"' 
 """
 }
