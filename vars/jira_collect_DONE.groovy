4a5a07cb3cabb15ee2720ab0556856c9512b2abc
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 18:08:24 +0530

    Create jira_collect_DONE.groovy

---
+++ vars/jira_collect_DONE.groovy
@@ -0,0 +1,5 @@
+curl --request GET \
+  --url 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3DEDN%20AND%20(status%3DDONE)' \
+  --header 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
+  --header 'cache-control: no-cache' \
+  --header 'postman-token: 87efe61f-ee85-21b5-b79d-bdad2c36140b'
