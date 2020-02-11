c8da1d6614cde1d25d63a4ebf94ad3e574d5a7c9
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 12:18:33 +0530

    Create jira_create_subtask_file.groovy

---
+++ vars/jira_create_subtask_file.groovy
@@ -0,0 +1,71 @@
+import groovy.json.*
+
+@NonCPS
+create(String projectName){
+
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/QualityGateDetails.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def GateId = resultJson.id
+String b=jsonObj.alm.projects.project.issue_name
+String issueName=b.replaceAll("\\[", "").replaceAll("\\]","");
+ 
+def length = 3
+def projLength = name.size()
+ println(projLength)
+ if(projLength>=3){
+  key=name.substring(0, Math.min(projLength, length)).toUpperCase();
+ }
+ else {
+   def appendStr = "";
+   int currentLength=length;
+   int len = currentLength%projLength;
+   int repeat = currentLength/projLength;
+   for (int i=0;i<repeat;i++) {
+    appendStr = appendStr + name;
+   }
+   appendStr=appendStr+name.substring(0, Math.min(projectName.length(), len));
+   key=appendStr.toUpperCase();
+   }
+ def projKey = key
+ 
+ println(projKey)
+ 
+ httpRequest authentication: 'jira_password',
+ customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'],
+                 [maskValue: false, name: 'Accept', value: 'application/json']], 
+ httpMode: 'POST', requestBody: """{
+    "fields":
+    {
+        "project":
+        {
+            "key": "${projKey}"
+        },
+        "parent":
+        {
+            "key": "${issueName}"
+        },
+        "summary": "Sub-task of TEST-101",
+        "description": "Don't forget to do this too.",
+        "issuetype":
+        {
+            "name":"Sub-task"
+        }
+    }
+}""", url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/'
+ 
+ }
+ 
+ 
+ 
+ 
+ def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
+
+String a=jsonObj.alm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+
+create(metric,operator,warning,error)
