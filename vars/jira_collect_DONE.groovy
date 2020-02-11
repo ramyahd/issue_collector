f8ff08edb509b89ffd106852d2d610122d09a4e0
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-11 18:12:34 +0530

    Update jira_collect_DONE.groovy

--- vars/jira_collect_DONE.groovy
+++ vars/jira_collect_DONE.groovy
@@ -13,7 +13,6 @@ env.name = projectName
 sh """curl --request GET \
   --url 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D'${projectName}'%20AND%20(status%3DDONE)' \
   --header 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
-  --header 'cache-control: no-cache' \
-  --header 'postman-token: 87efe61f-ee85-21b5-b79d-bdad2c36140b'
+  --header 'cache-control: no-cache' | json_reformat
 """
 }
