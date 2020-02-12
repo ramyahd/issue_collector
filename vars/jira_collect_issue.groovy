573c7878a8fd4970fc9e9450c5b78cc3630b47aa
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 18:59:31 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -24,35 +24,3 @@ sh """curl  -X GET \
  
 } 
 
-@NonCPS
-def create(){
-  def jsonSlurper = new JsonSlurper()
-  //def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
-  
-  def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"))
-  
-  def total = resultJson.total
-  echo "=============================Total $total"
-  pushToInflux(total);
-  
-
- }
-
-
-def pushToInflux(totalIssues) {
-  
-  sh """
-    curl -w '%{http_code}' -s -i  -X POST \
-      'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' \
-      --data 'jira issues=${totalIssues}' > test2.txt
-  """
- def response =new File('/var/lib/jenkins/workspace/' + JOB_NAME + '/test2.txt').text
-  echo "======================== $response" 
-  
-  if (response == "204" || response == "200") {
-      echo "DATA PUSHED TO INFLUX DB"
-     } else {
-     
-       error("ERROR PUSHING DATA TO INFLUX")
-     }
-}
