29627bf2ca90915be42bad1116b63aba28f4995c
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 19:25:53 +0530

    Update jira_collect_DONE.groovy

--- vars/jira_collect_DONE.groovy
+++ vars/jira_collect_DONE.groovy
@@ -9,8 +9,9 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 env.name = projectName
 
- withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariable: 'password', usernameVariable:'username')])
+ 
 sh """
+  withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariable: 'password', usernameVariable:'username')])
   curl  -X GET  -u $username:$password \
   'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3DEDN%20AND%20(status%3DDONE)' \
 //-H 'authorization: jira_password' \
