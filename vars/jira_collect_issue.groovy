a253aed4bbc7c8f5da09c19ae24f6fcd6677ca39
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 16:45:02 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -36,7 +36,7 @@ def create(){
   //pushToInflux(total);
   
    sh """
-    curl -w %{http_code} -i -o statusCode.txt -X POST \
+    curl -w %{http_code} -i -o statusCode2.txt -X POST \
     'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' \
     --data 'jira issues=6'
   """
