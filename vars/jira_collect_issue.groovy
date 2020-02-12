727d6afcbc301c6011ff48df04aa7ac3dcf65b64
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 14:25:13 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -23,7 +23,7 @@ def process=sh """curl  -X GET \
  
 } 
 
-@NonCPS
+
 def create(){
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
