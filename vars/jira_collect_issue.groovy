b16caa9ff5f32a4dbf655a106a67efda3900705a
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 14:28:43 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -30,18 +30,15 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson.total
   echo "$total"
-def STATUS=sh """curl -w '%{http_code}' -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${total}' 
+def status=sh """curl -w '%{http_code}' -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${total}' 
 """
   
   echo "$STATUS"
-  if ( '$STATUS' == "204" )
-  {  
-  echo "Got 204! All done!"
-  }
-  else
-  {
-    echo "Got $STATUS :( Not done yet..."
-  }
+  if [[ $status == 204 ]]; then
+    echo "Got 204! All done!"
+   
+fi
+ 
   
  }
 
