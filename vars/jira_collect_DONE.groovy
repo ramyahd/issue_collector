419e0ed738e26013642f59f2933a1b152193a228
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 19:14:36 +0530

    Update jira_collect_DONE.groovy

--- vars/jira_collect_DONE.groovy
+++ vars/jira_collect_DONE.groovy
@@ -12,7 +12,7 @@ env.name = projectName
 
 sh """
   curl -X GET \
-  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3DEDN250%20AND%20(status%3DDONE)' \
+  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3DEDN%20AND%20(status%3DDONE)' \
   -H 'authorization: jira_password' \
   -H 'cache-control: no-cache' \
 """
