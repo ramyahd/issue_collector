8b2e834ee3f61f684d55852a0d5dc29bd2eca7c0
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 12:19:08 +0530

    Update jira_collect_issue.groovy

--- vars/jira_collect_issue.groovy
+++ vars/jira_collect_issue.groovy
@@ -32,6 +32,13 @@ def total = resultJson.total
   echo "$total"
 sh """curl -w '%{http_code}' -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${total}' 
 """
+sh""" if [ $STATUS -eq 200 ]; then
+    echo "Got 200! All done!"
+    break
+  else
+    echo "Got $STATUS :( Not done yet..."
+  fi
+ """
 }
 
 
