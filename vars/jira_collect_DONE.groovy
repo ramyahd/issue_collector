10d8473b0af40ec17eb4365c02f9cd9111539b72
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-13 13:23:01 +0530

    Update jira_collect_DONE.groovy

--- vars/jira_collect_DONE.groovy
+++ vars/jira_collect_DONE.groovy
@@ -12,10 +12,10 @@ env.name = projectName
   withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariable: 'password', usernameVariable:'username')]){
 def done =  sh """
  
-  curl -X GET \
+  (script: curl -X GET \
     -H -d -u $username:$password \
   'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20AND%20(status%3DDONE)' \
-  -H 'cache-control: no-cache' | json_reformat
+  -H 'cache-control: no-cache' returnStdout: true)
  
     """
     echo "$done"
