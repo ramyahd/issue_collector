4e96cfaf44c6b16e165d33bac6994d7873818b25
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-13 11:54:37 +0530

    Update jira_collect_DONE.groovy

--- vars/jira_collect_DONE.groovy
+++ vars/jira_collect_DONE.groovy
@@ -15,7 +15,7 @@ def done =  sh """
   curl  -w  -X GET \
     -H -d -u $username:$password \
   'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20AND%20(status%3DDONE)' \
-  -H 'cache-control: no-cache' | head -n 1 | grep "HTTP/1.[01] [23]..."
+  -H 'cache-control: no-cache' | head -n 1 | grep "HTTP/1.[01] [23].."
     """
     echo "$done"
   }
