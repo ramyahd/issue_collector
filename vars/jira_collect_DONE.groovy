3a20e3d4e05d82086e937482c76a04fb80612f5d
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-13 17:12:58 +0530

    Update jira_collect_DONE.groovy

--- vars/jira_collect_DONE.groovy
+++ vars/jira_collect_DONE.groovy
@@ -42,7 +42,7 @@ def pushToInflux(totaltasks) {
   sh """
     curl -w '%{http_code}' -s -i -o test.txt -X POST \
       'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' \
-      --data 'jira issues=${totaltasks}' > test2.txt
+      --data 'jira tasks_done=${totaltasks}' > test2.txt
   """
  def response =new File('/var/lib/jenkins/workspace/' + JOB_NAME + '/test2.txt').text
   echo "======================== $response" 
