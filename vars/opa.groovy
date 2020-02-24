3d02a5a72a7118f9548c63d6aab72b44f5032a08
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-24 12:23:22 +0530

    Update opa.groovy

--- vars/opa.groovy
+++ vars/opa.groovy
@@ -1,6 +1,10 @@
 @NonCPS
 def getBuildUser() {
-    return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+    //return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+    def build = currentBuild.rawBuild
+def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+def name = cause.getUserName()
+echo "User: " + name
 }
 
 
