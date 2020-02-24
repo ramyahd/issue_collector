63916997cd9c94f331802553d33dd30606226ae3
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-24 15:12:53 +0530

    Create opa-policy.groovy

---
+++ vars/opa-policy.groovy
@@ -0,0 +1,37 @@
+
+
+def call(){
+
+
+
+
+
+
+
+
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
+
+
+
+
+
+
+
+
+
+
+
+sh "echo '${userid}'"
+
+
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "access": "build" } }'""", returnStdout: true)
+println(response)
+if ( response == "{\"result\":true}" ){
+println("You can build a job")
+
+}
+else{
+error("Build failed beacuse you do not have permission to trigger a build")
+}
+}
