0b131270aa932d55ba7328bc0bc3910ec84b09a5
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-25 12:00:22 +0530

    Update opa-policy.groovy

--- vars/opa-policy.groovy
+++ vars/opa-policy.groovy
@@ -1,35 +1,18 @@
-@NoNCPS
-def getBuildUser()
-{
-    
-    def build = currentBuild.rawBuild
-    def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
-    def name = cause.getUserName()
-    sh "echo user: '${name}' > name.txt "
+@NonCPS
+//getting the id of user who started the build.
+def getuserid(){
+def build = currentBuild.rawBuild
+def cause = build.getCause(hudson.model.Cause.UserIdCause.class)
+def userid = cause.getUserName()
 }
+
 def call(){
-
-
-
-
-
-
-
-
 sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
 
+getuserid()
 
-
-
-
-
-
-
-
-
-
-sh "echo '${name}'"
+println(userid)
 
 
 String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "access": "build" } }'""", returnStdout: true)
