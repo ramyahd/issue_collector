d0a98a72549e5ffdfda09b777668300077f88a06
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-25 11:44:17 +0530

    Update sonarpolicy1.groovy

--- vars/sonarpolicy1.groovy
+++ vars/sonarpolicy1.groovy
@@ -15,8 +15,8 @@ getuserid()
 println(userid)
 
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonar/policy/gate' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "gate": "'${userid}'.quality-gate"} }'""", returnStdout: true)
-println(response)
+//String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonar/policy/gate' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "gate": "'${userid}'.quality-gate"} }'""", returnStdout: true)
+//println(response)
 /*if ( response == "{\"result\":true}" ){
 println("You can build a job")
 }
