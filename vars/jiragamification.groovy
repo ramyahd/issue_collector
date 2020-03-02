59b7b7e44022a53794ec3dd4d562658f5a5f8108
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-02 15:14:23 +0530

    Update jiragamification.groovy

--- vars/jiragamification.groovy
+++ vars/jiragamification.groovy
@@ -22,7 +22,7 @@ sh """
   }
 
 def jsonSlurper = new JsonSlurper()
-def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/result.json"))
+def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
 def total = resultJson.size
-echo "Total no.of commits in $repoName $total"
+ echo "Total no.of commits in ${repoName} $total
 }
