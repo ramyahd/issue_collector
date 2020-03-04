f2eb97606b3f6fdbd69bf6dc7c4ba1de5783d3e2
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-04 18:02:57 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -6,11 +6,11 @@ sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @opa/P
 
 String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": {"servers": [{"id": "alice","branch": ["master"]}]} }'""", returnStdout: true)
 println(response)
-if ( response == "{\"result\":true}" ){
+/*if ( response == "{\"result\":true}" ){
 println("You can build a job")
 
 }
 else{
 error("Build failed beacuse you do not have permission to trigger a build")
-}
+}*/
 }
