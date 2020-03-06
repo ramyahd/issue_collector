bd0b42e5c9252a6ca826afd55560ef1bbf464fe8
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-06 15:59:39 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -1,10 +1,10 @@
 
 def call(){
-sh "curl -X PUT http://18.221.205.57:8181/v1/data/pipeline/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
-sh "curl -X PUT http://18.221.205.57:8181/v1/policies/pipeline --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/metrics --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/metrics --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/pipeline/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input":{"servers": {"branch_name": "master","id": "Alice"}}}'""")
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/result' --header 'Content-Type: application/json' --data-raw '{ "input":{"servers": {"branch_name": "master","id": "Alice"}}}'"""  returnStdout: true)
 println(response)
 /*if ( response == "{\"result\":true}" ){
 println("You can build a job")
