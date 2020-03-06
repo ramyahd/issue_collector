d10782c6cb79c0261ac8e658c12e391e48ccb45e
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-06 17:32:00 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -4,11 +4,13 @@ sh "curl -X PUT http://18.221.205.57:8181/v1/data/metrics --data-binary @opa/PIP
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/metrics --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
 
-String response = sh
-  """curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/result' \
-  --header 'Content-Type: application/json' 
+String response =
+  sh
+  """
+  curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/result' \
+  --header 'Content-Type: application/json' \
   --data-raw '{"servers": {"branch_name": "master","name": "Alice"}}' \
-  | jq
+  
   """
 println(response)
 /*if ( response == "{\"result\":true}" ){
