3321be72ddd44f8b141360b523056ee7b7cfee7f
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-06 17:28:42 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -4,7 +4,12 @@ sh "curl -X PUT http://18.221.205.57:8181/v1/data/metrics --data-binary @opa/PIP
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/metrics --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/result' --header 'Content-Type: application/json' --data-raw '{"servers": {"branch_name": "master","name": "Alice"}}'"""| jq)
+String response = sh
+  """curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/result' \
+  --header 'Content-Type: application/json' 
+  --data-raw '{"servers": {"branch_name": "master","name": "Alice"}}' \
+  | jq
+  """
 println(response)
 /*if ( response == "{\"result\":true}" ){
 println("You can build a job")
