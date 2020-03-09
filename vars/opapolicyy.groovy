58371e60c400da927933cec38a986e82f0e34e2e
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-09 14:31:00 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -1,12 +1,12 @@
 
 def call(){
-sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
-sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi1/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi1 --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
   
 sh """
   curl -X POST \
-  http://18.221.205.57:8181/v1/data/myapi/policy/result \
+  http://18.221.205.57:8181/v1/data/myapi1/policy/result \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -d '{"servers": {"branch_name": "master","name": "Alice"}}'
