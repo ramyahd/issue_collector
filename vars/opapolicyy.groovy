7159b94e39ca742a50aa9672a7f7a8cb103aa695
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-11 12:49:01 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -3,8 +3,57 @@ def call(){
 //sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi1/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi1 --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
+
   
-sh """
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/myapi1/policy/foo' --header 'Content-Type: application/json' --data-raw '{ "servers": [
+        {
+            "branch_name": "master",
+            "metrics": [
+                {
+                    "code-coverage": "50"
+                }
+            ],
+            "name": "Alice"
+        },
+        {
+            "branch_name": "feature",
+            "metrics": [
+                {
+                    "code-coverage": "85"
+                }
+            ],
+            "name": "Alice"
+        },
+        {
+            "branch_name": "master",
+            "metrics": [
+                {
+                    "code-coverage": "60"
+                }
+            ],
+            "name": "Bob"
+        },
+        {
+            "branch_name": "feature",
+            "metrics": [
+                {
+                    "code-coverage": "80"
+                }
+            ],
+            "name": "Bob"
+        }
+    ]} }'""", returnStdout: true)
+println(response)
+if ( response == "{\"result\":true}" ){
+println("You can build a job")
+
+}
+else{
+error("Build failed beacuse you do not have permission to trigger a build")
+}
+}
+
+/*sh """
   curl -X POST -s \
   http://18.221.205.57:8181/v1/data/myapi1/policy/foo \
   -H 'cache-control: no-cache' \
@@ -49,8 +98,8 @@ sh """
         }
     ]}'
   
-  """
-//println(response)
+  """*/
+println(response)
 /*if ( response == "{\"result\":true}" ){
 println("You can build a job")
 
