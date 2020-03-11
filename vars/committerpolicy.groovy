a65729b683e174ed3dca4c81dbecb20968722549
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-11 15:46:18 +0530

    Update committerpolicy.groovy

--- vars/committerpolicy.groovy
+++ vars/committerpolicy.groovy
@@ -1,6 +1,6 @@
 def call(committername,branch){
 sh "curl -X PUT http://18.221.205.57:8181/v1/data/metrics/acl --data-binary @open-policy-agent/METRICS/COMMITTER_BASED/data.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/metrics --data-binary @open-policy-agent/METRICS/COMMITTER_BASED/metrics.rego"
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/play/foo' --header 'Content-Type: application/json' --data-raw '{"input":[{"name":"'${committername}'","branch_name":"'${branch}'"}]}'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/metrics/policy/foo' --header 'Content-Type: application/json' --data-raw '{"input":[{"name":"'${committername}'","branch_name":"'${branch}'"}]}'""", returnStdout: true)
 println(response)
 }
