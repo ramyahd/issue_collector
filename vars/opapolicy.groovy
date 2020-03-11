f10c2c7b6a9d3caa2ce2e06be1bbcbb03924a278
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-11 15:15:31 +0530

    Update opapolicy.groovy

--- vars/opapolicy.groovy
+++ vars/opapolicy.groovy
@@ -7,15 +7,15 @@ def userid = cause.getUserName()
 }
 
 def call(){
-sh "curl -X PUT http://18.221.205.57:8181/v1/data/jen/acl --data-binary @opa/JENKINS/BuildPolicy/jenkins-acl.json"
-sh "curl -X PUT http://18.221.205.57:8181/v1/policies/jen --data-binary @opa/JENKINS/BuildPolicy/jenkins-policy.rego"
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/myapi/acl --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-acl.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/myapi --data-binary @open-policy-agent/JENKINS/BuildPolicy/jenkins-policy.rego"
 
 getuserid()
 
 println(userid)
 
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/jen/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "access": "build" } }'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/myapi/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "access": "build" } }'""", returnStdout: true)
 println(response)
 if ( response == "{\"result\":true}" ){
 println("You can build a job")
