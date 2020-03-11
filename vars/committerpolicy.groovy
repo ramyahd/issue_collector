1fc1180abe9b89364ff502e3c1f708d29730522b
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-11 18:59:00 +0530

    Update committerpolicy.groovy

--- vars/committerpolicy.groovy
+++ vars/committerpolicy.groovy
@@ -1,6 +1,16 @@
 def call(committername,branch){
 sh "curl -X PUT http://18.221.205.57:8181/v1/data/commit/acl --data-binary @open-policy-agent/METRICS/COMMITTER_BASED/data.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/commit --data-binary @open-policy-agent/METRICS/COMMITTER_BASED/metrics.rego"
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/commit/policy/foo' --header 'Content-Type: application/json' --data-raw '{"input":{"name":"'${committername}'","branch_name":"'${branch}'"}}'""", returnStdout: true)
-println(response)
+//String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/commit/policy/foo' --header 'Content-Type: application/json' --data-raw '{"input":{"name":"'${committername}'","branch_name":"'${branch}'"}}'""", returnStdout: true)
+//println(response)
+  
+  sh """
+  curl -X POST \
+  http://18.221.205.57:8181/v1/data/commit/policy/foo \
+  --data-binary '{"input":{"name":"'${committername}'","branch_name":"'${branch}'"} }' | json_reformat
+  
+  """
+
 }
+
+
