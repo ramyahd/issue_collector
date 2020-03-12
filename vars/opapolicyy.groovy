a5bc72b5f248712d562902685d5d9f8fafe8fcb6
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-12 14:56:51 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -1,9 +1,9 @@
 
 def call(){
-sh "curl -X PUT http://13.234.225.51:8181/v1/data/jenkinss/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
-sh "curl -X PUT http://13.234.225.51:8181/v1/policies/jenkinss --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/jenkinss/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/jenkinss --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
-String response = sh(script:"""curl --location --request POST 'http://13.234.225.51:8181/v1/data/jenkinss/policy/foo' --header 'Content-Type: application/json' --data-raw '{ "input" : { "branch_name": "master","name": "Alice" } }'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/jenkinss/policy/foo' --header 'Content-Type: application/json' --data-raw '{ "input" : { "branch_name": "master","name": "Alice" } }'""", returnStdout: true)
 println(response)
 /*sh """
   curl -X POST \
