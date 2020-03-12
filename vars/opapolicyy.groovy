5a0d66f815b753cd1598452ea3edabadc03dbe47
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-12 13:13:53 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -3,61 +3,14 @@ def call(){
 sh "curl -X PUT http://13.234.225.51:8181/v1/data/jenkinss/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
 sh "curl -X PUT http://13.234.225.51:8181/v1/policies/jenkinss --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
 
-
-  
-/*String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/jenkinss/policy/foo' --header 'Content-Type: application/json' --data-raw '{ "servers": [
-        {
-            "branch_name": "master",
-            "metrics": [
-                {
-                    "code-coverage": "50"
-                }
-            ],
-            "name": "Alice"
-        },
-        {
-            "branch_name": "feature",
-            "metrics": [
-                {
-                    "code-coverage": "85"
-                }
-            ],
-            "name": "Alice"
-        },
-        {
-            "branch_name": "master",
-            "metrics": [
-                {
-                    "code-coverage": "60"
-                }
-            ],
-            "name": "Bob"
-        },
-        {
-            "branch_name": "feature",
-            "metrics": [
-                {
-                    "code-coverage": "80"
-                }
-            ],
-            "name": "Bob"
-        }
-    ] }'""", returnStdout: true)
-println(response)
-if ( response == "{\"result\":true}" ){
-println("You can build a job")
-
-}
-else{
-error("Build failed beacuse you do not have permission to trigger a build")
-}
-}*/
-sh """
+String response = sh(script:"""curl --location --request POST 'http://13.234.225.51:8181/v1/data/jenkinss/policy/foo' --header 'Content-Type: application/json' --data-raw '{ "input" : { "branch_name": "master","name": "Alice" } }'""", returnStdout: true)
+prinltn(response)
+/*sh """
   curl -X POST \
   http://13.234.225.51:8181/v1/data/jenkinss/policy/foo \
   --data-binary '{ "input" : { "branch_name": "master","name": "Alice" } }' | json_reformat
   
-  """
+  """*/
 //println(response)
 /*if ( response == "{\"result\":true}" ){
 println("You can build a job")
