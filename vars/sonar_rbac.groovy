3af173af8778609efa77e84ebcd6ed7465fe8b9e
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-12 18:22:54 +0530

    Update sonar_rbac.groovy

--- vars/sonar_rbac.groovy
+++ vars/sonar_rbac.groovy
@@ -1,1 +1,12 @@
-
+def call(){
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/sonarrbac/acl --data-binary @open-policy-agent/RBAC/ROLE_BASED/sonar-acl.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/sonarrbac --data-binary @open-policy-agent/SONARRBAC/ROLE_BASED/sonar-rbac.rego"
+//String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/commit/policy/foo' --header 'Content-Type: application/json' --data-raw '{"input":{"name":"'${committername}'","branch_name":"'${branch}'"}}'""", returnStdout: true)
+//println(response)
+  
+String response1 = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input" : { "user": "alice","path": "/master/"} }'""", returnStdout: true)
+println(response1)
+  
+String response2 = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/result' --header 'Content-Type: application/json' --data-raw '{ "input" : { "user": "alice","path": "/master/"} }'""", returnStdout: true)
+println(response2)  
+}
