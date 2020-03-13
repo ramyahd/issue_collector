9699ee7115b36308292361db0cefca614e0e7c1d
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-13 16:52:35 +0530

    Update sonar_rbac.groovy

--- vars/sonar_rbac.groovy
+++ vars/sonar_rbac.groovy
@@ -4,9 +4,17 @@ sh "curl -X PUT http://18.221.205.57:8181/v1/policies/sonarrbac --data-binary @o
 //String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/commit/policy/foo' --header 'Content-Type: application/json' --data-raw '{"input":{"name":"'${committername}'","branch_name":"'${branch}'"}}'""", returnStdout: true)
 //println(response)
   
-String response1 = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input" : { "user": "alice","branch": "master","auth": "ram"} }'""", returnStdout: true)
-println(response1)
-  
-String response2 = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/result' --header 'Content-Type: application/json' --data-raw '{ "input" : { "user": "alice","branch": "master", "auth": "ram"} }'""", returnStdout: true)
-println(response2)  
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/allow' --header 'Content-Type: application/json' --data-raw '{ "input" : {"auth": "ram"} }'""", returnStdout: true)
+println(response)
+if ( response == "{\"result\":true}" ){
+String response1 = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/result' --header 'Content-Type: application/json' --data-raw '{ "input" : { "user": "alice","branch": "master", "auth": "ram"} }'""", returnStdout: true)
+println(response1)  
+
+}
+else{
+String response2 = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarrbac/policy/deny' --header 'Content-Type: application/json' --data-raw '{ "input" : {"auth": "ram"} }'""", returnStdout: true)  
+println(response2)
+  error("you cannot see the result")
+}  
+
 }
