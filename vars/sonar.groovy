16e63df36bce463f12f183b97078e355173037cc
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-24 12:11:08 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,10 +1,3 @@
-def call(String msg = 'SONAR'){
-  echo "${msg}"
- 
-  environment{
-    scannerHome=tool 'sonarScanner'  
-  }
-  withSonarQubeEnv('sonarqube'){
-                     sh 'mvn sonar:sonar'
-                }
+def call(){
+def process=sh "curl --location --request GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI'  --header 'Authorization: Basic YWRtaW46YWRtaW4=' -o ouput.json"
 }
