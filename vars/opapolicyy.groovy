51768a11a5655d77cb28774673b57e037ad41782
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-30 11:12:09 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -1,9 +1,9 @@
 
 def call(){
-sh "curl -X PUT http://18.221.205.57:8181/v1/data/jenkinss/acl --data-binary @opa/PIPELINE/BuildPolicy/pipeline-acl.json"
-sh "curl -X PUT http://18.221.205.57:8181/v1/policies/jenkinss --data-binary @opa/PIPELINE/BuildPolicy/pipeline-policy.rego"
+sh "curl -X PUT http://18.221.205.57:8181/v1/data/sonarcode/acl --data-binary @open-policy-agent/SONAR/quality-gate/sonar-gate.json"
+sh "curl -X PUT http://18.221.205.57:8181/v1/policies/sonarcode --data-binary @open-policy-agent/SONAR/quality-gate/sonar-policy.rego"
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/jenkinss/policy/foo' --header 'Content-Type: application/json' --data-raw '{ "input" : [{ "branch_name": "master","name": "Alice" }] }'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarcode/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"project": "Wolvorines","branch": "Metrics", "auth" : "ramya", "vulnerability": "1","codecoverage":"90"} }'""", returnStdout: true)
 println(response)
 /*sh """
   curl -X POST \
