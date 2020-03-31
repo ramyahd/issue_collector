49bbef8b9b0f5a519fda5f77427a14e388631f5e
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-03-31 10:59:20 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -3,7 +3,7 @@ def call(){
 sh "curl -X PUT http://18.221.205.57:8181/v1/data/sonarcode/acl --data-binary @open-policy-agent/SONAR/quality-gate/sonar-gate.json"
 sh "curl -X PUT http://18.221.205.57:8181/v1/policies/sonarcode --data-binary @open-policy-agent/SONAR/quality-gate/sonar-policy.rego"
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarcode/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"project": "Wolvorines","branch": "Metrics", "auth" : "ramya", "vulnerability": "2","codecoverage":"90"} }'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonarcode/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"project": "Wolvorines","branch": "Metrics", "auth" : "ramya", "vulnerability": "1","codecoverage":"90"} }'""", returnStdout: true)
 println(response)
 /*sh """
   curl -X POST \
