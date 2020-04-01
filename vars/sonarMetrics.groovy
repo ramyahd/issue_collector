f54b73187f967c6a62f6baf0c40d67e29b7bf35f
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-04-01 12:32:02 +0530

    Update sonarMetrics.groovy

--- vars/sonarMetrics.groovy
+++ vars/sonarMetrics.groovy
@@ -25,7 +25,7 @@ String ProjectName=a.replaceAll("\\[", "").replaceAll("\\]","");
 withCredentials([usernamePassword(credentialsId: 'sonar_cred1', passwordVariable: 'pass', usernameVariable: 'user')]) {
 sh "curl -u ${user}:${pass} -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=${ProjectName}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metricss.json"
   echo 'metrics collected'
-  println(response)
+ // println(response)
 }
 //  create()
 }
