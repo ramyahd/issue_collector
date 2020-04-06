8b5cbe578f36cbfc0e95e0af22c8a337f70cb813
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-04-06 17:57:29 +0530

    Update opa_gitlab.groovy

--- vars/opa_gitlab.groovy
+++ vars/opa_gitlab.groovy
@@ -15,3 +15,10 @@ String response = sh(script:"""curl --location --request POST 'http://18.224.228
 println(response)
 
 }
+if ( response == "{"result"/"allow"/true}" ){
+println("You can build a job")
+
+}
+else{
+error("Build failed beacuse you do not have permission to trigger a build")
+}
