a790970e9462e2ccff9b3a470731d2957589ffe2
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-04-06 19:19:50 +0530

    Update opa_gitlab.groovy

--- vars/opa_gitlab.groovy
+++ vars/opa_gitlab.groovy
@@ -17,7 +17,7 @@ String response = sh(script:"""curl --location --request POST 'http://18.224.228
 } }'""", returnStdout: true)
 println(response)
 def resultJson= readJSON text: response	
-println(resultJson)
+println(resultJson.result.allow)
 
 //try
 //{
