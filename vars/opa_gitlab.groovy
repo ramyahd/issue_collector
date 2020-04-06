a1db4f0e9404e6c98c2c0038b8840cec570c2bb9
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-04-06 18:59:16 +0530

    Update opa_gitlab.groovy

--- vars/opa_gitlab.groovy
+++ vars/opa_gitlab.groovy
@@ -1,6 +1,4 @@
   
-import groovy.json.*
-import groovy.json.JsonOutput
 @NonCPS
 //getting the id of user who started the build.
 def getuserid(){
@@ -15,22 +13,22 @@ sh "curl -X PUT http://18.224.228.236:8181/v1/policies/gitlab --data-binary @ope
 
 String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/gitlab/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"ticker": "master","user": "'${userid}'"
 } }'""", returnStdout: true)
-//println(response)
- try
- {
-def jsonSlurper = new JsonSlurper()
+println(response)
+//try
+//{
+//def jsonSlurper = new JsonSlurper()
 //f reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/rigoutput.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(response)
-println(resultJson.result.allow)
+//def resultJson = jsonSlurper.parse(response)
+//println(resultJson.result.allow)
 //return JsonOutput.toJson(resultJson.result)
 }
-  catch(Exception e)
-{
+  //catch(Exception e)
+//{
 	//println(response)
-	e.printStackTrace()
+//	e.printStackTrace()
 	
-}
-}
+//}
+//}
   
 //if ( response == {/"result/":{/"allow/":false,/"object_attributes/":{/"feature/":{/"devcan/":"merge",/"permission/":"granted"},/"master/":{"admincan":"merge","permission":"granted"}},"user_attributes":{"alice":{"commits":15,"role":"admin"},"bob":{"commits":5,"role":"developer"}}}}){    
 //println("You can build a job")
