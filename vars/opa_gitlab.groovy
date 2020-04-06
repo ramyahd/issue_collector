78f111498a336c64f681bc7710cd468681f2b555
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-04-06 17:54:05 +0530

    Update opa_gitlab.groovy

--- vars/opa_gitlab.groovy
+++ vars/opa_gitlab.groovy
@@ -10,7 +10,7 @@ def call(){
 sh "curl -X PUT http://18.224.228.236:8181/v1/data/gitlab/acl --data-binary @open-policy-agent/GITLAB/gitlab_data.json"
 sh "curl -X PUT http://18.224.228.236:8181/v1/policies/gitlab --data-binary @open-policy-agent/GITLAB/gitlab_policy.rego"
 
-String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/gitlab/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"ticker": "master","user": "alice"
+String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/gitlab/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"ticker": "master","user": "'${userid}'"
 } }'""", returnStdout: true)
 println(response)
 
