b77a95c5e240b85b7b6b10b05e9e347e69667bf5
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-04-06 16:21:49 +0530

    Update opapolicyy.groovy

--- vars/opapolicyy.groovy
+++ vars/opapolicyy.groovy
@@ -5,19 +5,5 @@ sh "curl -X PUT http://18.224.228.236:8181/v1/policies/sonarcode --data-binary @
 
 String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/sonarcode/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"project": "Wolvorines","branch": "Metrics", "auth" : "ramya", "vulnerability": "1","codecoverage":"90"} }'""", returnStdout: true)
 println(response)
-/*sh """
-  curl -X POST \
-  http://13.234.225.51:8181/v1/data/jenkinss/policy/foo \
-  --data-binary '{ "input" : { "branch_name": "master","name": "Alice" } }' | json_reformat
-  
-  """*/
-//println(response)
-/*if ( response == "{\"result\":true}" ){
-println("You can build a job")
-
-}
-else{
-error("Build fail beacuse you do not have permission to trigger a build")
-}*/
 }
 
