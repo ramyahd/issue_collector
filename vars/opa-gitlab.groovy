68e4c7ef249dc62890f4c76e3307f2b758e07cab
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-04-06 17:29:18 +0530

    shared lib  updated

--- vars/opa-gitlab.groovy
+++ vars/opa-gitlab.groovy
@@ -1,6 +1,7 @@
 def call(){
-sh "curl -X PUT http://18.224.228.236:8181/v1/data/sonarcode/acl --data-binary @open-policy-agent/SONAR/quality-gate/sonar-gate.json"
-sh "curl -X PUT http://18.224.228.236:8181/v1/policies/sonarcode --data-binary @open-policy-agent/SONAR/quality-gate/sonar-policy.rego"
+sh "curl -X PUT http://18.224.228.236:8181/v1/data/gitlab/acl --data-binary @open-policy-agent/GITLAB/gitlab_data.json"
+sh "curl -X PUT http://18.224.228.236:8181/v1/policies/gitlab --data-binary @open-policy-agent/GITLAB/gitlab_policy.rego"
 
-String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/sonarcode/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"project": "Wolvorines","branch": "Metrics", "auth" : "ramya", "vulnerability": "1","codecoverage":"90"} }'""", returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/gitlab/policy' --header 'Content-Type: application/json' --data-raw '{ "input" : {"ticker": "master","user": "alice"
+} }'""", returnStdout: true)
 println(response)
