c4d0481d08f3f6ea316599527485cf5ecfe92669
ramyahd <55663832+ramyahd@users.noreply.github.com>
2021-01-24 19:20:53 +0530

    Update sonarpolicy1.groovy

--- vars/sonarpolicy1.groovy
+++ vars/sonarpolicy1.groovy
@@ -7,15 +7,15 @@ def userid = cause.getUserName()
 }
 
 def call(){
-sh "curl -X PUT http://18.221.205.57:8181/v1/data/sonar/acl --data-binary @opa/SONAR/BuildPolicy/sonargate.json"
-sh "curl -X PUT http://18.221.205.57:8181/v1/policies/sonar --data-binary @opa/SONAR/BuildPolicy/sonar-policy.rego"
+sh "curl -X PUT http://18.224.228.236:8181/v1/data/sonar/acl --data-binary @opa/SONAR/BuildPolicy/sonargate.json"
+sh "curl -X PUT http://18.224.228.236:8181/v1/policies/sonar --data-binary @opa/SONAR/BuildPolicy/sonar-policy.rego"
 
 getuserid()
 
 println(userid)
 
 
-String response = sh(script:"""curl --location --request POST 'http://18.221.205.57:8181/v1/data/sonar/policy/gate' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "qualitygate": "50"} }'""" , returnStdout: true)
+String response = sh(script:"""curl --location --request POST 'http://18.224.228.236:8181/v1/data/sonar/policy/gate' --header 'Content-Type: application/json' --data-raw '{ "input": { "user": "'${userid}'", "qualitygate": "50"} }'""" , returnStdout: true)
 println(response)
 /*if ( response == "{\"result\":true}" ){
 println("You can build a job")
